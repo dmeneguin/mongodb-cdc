49e226489f37f630ce177ad60490b1cb1638e2ae
dmeneguin <meneguin.daniel@gmail.com>
2023-03-08 10:25:25 -0300

    added cdc listener execution on start script

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DELAY=10
+DELAY_LISTENER=30
 
 docker compose -f docker-compose.yml down
 docker rm -f $(docker ps -a -q)
@@ -12,3 +13,6 @@ echo "****** Waiting for ${DELAY} seconds for containers to go up ******"
 sleep $DELAY
 
 docker exec mongo1 /scripts/rs-init.sh
+
+echo "STARTING CDC LISTENER..."
+docker compose -f docker-compose-listener.yml up -d
