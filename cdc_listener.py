2b3ea3515d810b7d5938d4d24086b9eeffd84a99
dmeneguin <meneguin.daniel@gmail.com>
2023-03-16 23:17:34 -0300

    added file log

--- cdc_listener.py
+++ cdc_listener.py
@@ -1,14 +1,17 @@
 import pymongo
+import logging
+
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
 
 db = pymongo.MongoClient('mongodb://mongo1:27017/')
 db = db.test
-print('conectando...')
+logging.info('conectando...')
 try:
     resume_token = None
     #pipeline = [{'$match': {'operationType': 'insert'}}]
     with db.jorge.watch() as stream:
         for insert_change in stream:
-            print(insert_change)
+            logging.info(insert_change)
             resume_token = stream.resume_token
 except pymongo.errors.PyMongoError as e:
     # The ChangeStream encountered an unrecoverable error or the
@@ -16,12 +19,12 @@ except pymongo.errors.PyMongoError as e:
     if resume_token is None:
         # There is no usable resume token because there was a
         # failure during ChangeStream initialization.
-        print(e)
+        logging.info(e)
     else:
         # Use the interrupted ChangeStream's resume token to create
         # a new ChangeStream. The new stream will continue from the
         # last seen insert change without missing any events.
-        print('??????')
+        logging.info('??????')
         with db.collection.watch(
                 pipeline, resume_after=resume_token) as stream:
             for insert_change in stream:
